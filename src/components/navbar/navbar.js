abf8e62e32137daf178a3d68a53c5d8375331eee
Yunus Emre TURHAN <emreturhanofficial34@gmail.com>
2024-09-13 01:52:21 +0300

    efe

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,7 +5,7 @@ const navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container">
-  <Link to ="/" className="fs-3 text-center ubuntu navbar-brand">Rick And <span className="text-primary">Morty</span></Link>
+  <Link to ="/" className="fs-3 text-center ubuntu navbar-brand">Rick And Morty <span className="text-primary">Morty</span></Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
